d3e9006bfda05045c1111a8fdca3c257542f67ad
Armando García Paniagua <armandgp07@gmail.com>
2022-05-09 00:16:48 -0600

    Avance 3
    Pasos 19 a 22

--- web-api/src/index.js
+++ web-api/src/index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 9000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
